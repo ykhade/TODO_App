e813d6743ad94764424261f9e843fae2d93477ba
yashkhade <ykhade@nevada.unr.edu>
2019-04-22 20:45:23 -0700

    Created UI for app bar

--- lib/main.dart
+++ lib/main.dart
@@ -28,13 +28,51 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    var appColors = [Color.fromRGBO(231, 129, 109, 1.0),Color.fromRGBO(99, 138, 223, 1.0),Color.fromRGBO(111, 194, 173, 1.0)];
+    var cardIndex = 0;
+
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("TODO"),
+        title: new Text("TODO", style: TextStyle(fontSize: 16.0),),
+        backgroundColor: appColors[cardIndex],
         centerTitle: true,
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Icon(Icons.search),
+          )
+        ],
+        elevation: 0.0,
       ),
       body: new Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 64.0, vertical: 32.0),
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Icon(Icons.account_circle, size: 45.0, color: Colors.white,),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0.0,16.0,0.0,12.0),
+                      child: Text("Hello", style: TextStyle(fontSize: 30.0, color: Colors.white),),
+                    ),
+                    Text("Looks like feel good.", style: TextStyle(color: Colors.white),),
+                    Text("You have 2 tasks to do today.", style: TextStyle(color: Colors.white,),),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
+      drawer: Drawer(),
     );
   }
 }
